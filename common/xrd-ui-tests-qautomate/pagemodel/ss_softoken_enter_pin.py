0bae19157c9d48604c7f15192f09c7d7736c3222
lasse.matikainen <lasse.matikainen@qautomate.fi>
2018-04-12 17:44:04 +0300

    Fixed import

--- common/xrd-ui-tests-qautomate/pagemodel/ss_softoken_enter_pin.py
+++ common/xrd-ui-tests-qautomate/pagemodel/ss_softoken_enter_pin.py
@@ -2,6 +2,7 @@
 # Example for using WebDriver object: driver = get_driver() e.g driver.current_url
 from webframework import TESTDATA
 from selenium.webdriver.common.by import By
+from webframework.extension.util.common_utils import *
 from time import sleep
 
 class Ss_softoken_enter_pin(CommonUtils):
